11ebe3b7be7c3d796d174e7b9509fea7df4ae217
Liam Simons <liam.simons@student.kuleuven.be>
2020-10-18 15:45:53 +0200

    Untracked files - moet nog verwijderd worden

---
+++ lab1/ex3/main.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+//void upper_string(char [])
+
+int main()
+{
+	int MAX = 10;
+	char first[MAX + 1], second[MAX + 1],
+	/* str[2*MAX + 1],*/ name[2*MAX + 1];
+	printf("Enter your first name: ");
+	scanf("%s", first);
+	printf("Enter your second name: ");
+	scanf("%s", second);
+
+//	printf("%s %s (1)\n", first, second);
+//	char *strcpy(char *str, const char *second);
+//	printf("%s (2)\n", &str);
+	strcat(name, first);
+	strcat(name, second);
+
+	printf("%s\n %s\n %s\n", first, second, name);
+	return 0;
+}
+
